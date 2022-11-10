0fde6a4fb14a4ab30549580db9804e0ef51481d2
Nick Prater <NikPratr@gmail.com>
2022-11-09 22:41:45 -0500

    finished query and inquirer functionality

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const inquirer = require('inquirer');
-// const sequelize = require('./config/connections.js');
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
@@ -11,7 +10,7 @@ const db = mysql.createConnection({
 
 db.connect(function (err) {
     if (err) throw err;
-    startMyApp()
+    startMyApp();
 });
 
 console.log("\n- - - Ｅｍｐｌｏｙｅｅ - Ｔｒａｃｋｅｒ - - -\n");
@@ -50,7 +49,7 @@ function startMyApp() {
             viewDepts();
         } else if(choice.question == "Add Department") {
             addDept();
-        }
+        } else quit();
     });
 }
 
@@ -80,6 +79,130 @@ function viewEmp() {
     })
 };
 
+function addEmployee() {
+    const getRoles = `SELECT * FROM roles;`
+    const roleList = [];
+
+    db.query(getRoles, (err, data) => {
+        if (err) throw err;
+        for(i=0; i < data.length; i++) {
+            const obj1 = {
+                name: data[i].title,
+                value: data[i].id
+            }
+            roleList.push(obj1);
+        }
+    })
+    
+    const getEmps = `SELECT * FROM employees;`
+    const EmpsList = [];
+
+    db.query(getEmps, (err, data) => {
+        if (err) throw err;
+        for(i=0; i < data.length; i++) {
+            const obj1 = {
+                name: data[i].first_name + ' ' + data[i].last_name,
+                value: data[i].id
+            }
+            EmpsList.push(obj1);
+        }
+    })
+
+    inquirer.prompt([
+        {
+            message: "Enter the employee's first name",
+            name: "fName"
+        },
+        {
+            message: "Enter the employee's last name",
+            name: "lName"
+        },
+        {
+            type: 'list',
+            message: "Please select employee's role",
+            name: 'role',
+            choices: roleList,
+        },
+        {
+            type: 'list',
+            message: "Please select employee's Manager",
+            name: 'manager',
+            choices: EmpsList
+        }
+    ]).then (answers => {
+        
+            const sqlString=`
+            INSERT INTO employees(first_name, last_name, role_id, manager_id)
+            VALUES (?, ?, ?, ?)`
+    
+            console.log(answers);
+    
+                db.query(sqlString, [answers.fName, answers.lName, answers.role, answers.manager], function (err, data) {
+                    if (err) throw err;
+                    console.log("\nNew employee added\n");
+                    startMyApp();
+                });
+        })
+};
+
+function updateEmployee() {
+    const getEmps = `SELECT * FROM employees;`
+    const EmpsList = [];
+
+    db.query(getEmps, (err, data) => {
+        if (err) throw err;
+        for(i=0; i < data.length; i++) {
+            const obj1 = {
+                name: data[i].first_name + ' ' + data[i].last_name,
+                value: data[i].id
+            }
+            EmpsList.push(obj1);
+        }
+
+        const getRoles = `SELECT * FROM roles;`
+        const roleList = [];
+
+        db.query(getRoles, (err, data) => {
+            if (err) throw err;
+            for(i=0; i < data.length; i++) {
+                const obj1 = {
+                    name: data[i].title,
+                    value: data[i].id
+                }
+                roleList.push(obj1);
+            }
+
+            inquirer.prompt([
+                {
+                    type: "list",
+                    message: "Which employee would you like to update?",
+                    name: 'emp',
+                    choices: EmpsList
+                },
+                {
+                    type: "list",
+                    message: "What role would you like to assign?",
+                    name: 'role',
+                    choices: roleList
+                },
+            ]).then((answers) => {
+                const sqlString=
+                `UPDATE employees
+                SET role_id = ?
+                WHERE id = ?`;
+                console.log(answers);
+
+                db.query(sqlString, [answers.role, answers.emp], function (err, data) {
+                    if (err) throw err;
+                    console.log("\nRole updated\n");
+                    startMyApp();
+                })
+            })
+        })
+    })
+
+};
+
 function viewRoles() {
     const sqlString =
     `SELECT * FROM roles;`;
@@ -106,16 +229,19 @@ function viewDepts() {
 };
 
 function addRole() {
-
     const getDepts = `SELECT * FROM departments;`
-    const deptList = []; 
+    const deptList = [];
 
     db.query(getDepts, (err, data) => {
         if (err) throw err;
         for(i=0; i < data.length; i++) {
-            deptList.push(data[i].dep_name);
+            const obj = {
+                name: data[i].dep_name,
+                value: data[i].id
+            }
+            deptList.push(obj);
         }
-    })
+    });
 
     inquirer.prompt([
         {
@@ -138,25 +264,15 @@ function addRole() {
         INSERT INTO roles(title, salary, dep_id)
         VALUES (?, ?, ?)`
 
-        db.query(getDepts, (err, data) => {
-            if (err) throw err;
-            for(i=0; i < data.length; i++) {
-                if(roleAnswers.roleDept === data[i].dep_name) {
-                    roleAnswers.roleDept = data[i].id;
-                }
-            }
-        })
+        console.log(roleAnswers);
 
-        
-        setTimeout(() => {
             db.query(sqlString, [roleAnswers.roleName, roleAnswers.roleSalary, roleAnswers.roleDept], function (err, data) {
                 if (err) throw err;
                 console.log("\nAdded new role\n");
                 startMyApp();
-            })
-        }, 20);
+            });
     })
-}
+};
 
 function addDept() {
     inquirer.prompt([
@@ -176,4 +292,9 @@ function addDept() {
             startMyApp();
         })
     })
+};
+
+function quit() {
+    console.log("Goodbye");
+    process.exit();
 };
